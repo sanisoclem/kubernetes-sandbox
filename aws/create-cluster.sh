83907e9a22fe6cbccfae57910b1ac87402f2ac9c
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2018-02-08 03:39:39 +0800

    stufff

--- aws/create-cluster.sh
+++ aws/create-cluster.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+set -e
 
-$CLUSTER_NAME = "sanisoclem-k8s"
+export CLUSTER_NAME=sanisoclem-01
+export BUCKET_NAME=${CLUSTER_NAME}-k8s-state-store
+export KOPS_STATE_STORE=s3://$BUCKET_NAME
+export NAME=${CLUSTER_NAME}.k8s.local
 
 aws configure           # Use your new access and secret key here
 aws iam list-users      # you should see a list of all your IAM users here
@@ -12,24 +16,19 @@ export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
 
 # create an s3 bucket for config
 
-aws s3api create-bucket --bucket "${CLUSTER_NAME}-state-store" \
-     --region ap-southeast-1 --create-bucket-configuration LocationConstraint=ap-southeast-1
+aws s3api create-bucket --bucket "$BUCKET_NAME" --region ap-southeast-1 --create-bucket-configuration LocationConstraint=ap-southeast-1
 
 # enable versioning 
- aws s3api put-bucket-versioning --bucket "${CLUSTER_NAME}-state-store" \
-     --versioning-configuration Status=Enabled
+ aws s3api put-bucket-versioning --bucket "$BUCKET_NAME" --versioning-configuration Status=Enabled
 
 
-export KOPS_STATE_STORE=s3://${CLUSTER_NAME}-state-store
-export NAME=$CLUSTER_NAME
+#aws ec2 describe-availability-zones --region ap-southeast-1
 
-#aws ec2 describe-availability-zones --region ap-southease-1
-
- kops create cluster --zones ap-southeast-1a ${CLUSTER_NAME}
- kops update cluster ${CLUSTER_NAME} --yes
+ kops create cluster --zones ap-southeast-1a ${NAME}
+ kops update cluster ${NAME} --yes
 
 # create dashboard
 kubectl create -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/kubernetes-dashboard/v1.8.1.yaml
 
 // gets user password
-kops get secrets kube --type secret -oplaintext
+#kops get secrets kube --type secret -oplaintext
